407e83007a1fcab7bc6984e4fba3f4d06ccf2bc3
Adrien Bourgois <a.bourgois@student.isartdigital.com>
2015-10-07 17:04:44 +0200

    [EDITOR] Add other fields and move buttons

--- editor/src/context.cpp
+++ editor/src/context.cpp
@@ -35,9 +35,30 @@ void SAppContext::createPlayerDialog()
     this->Gui->addStaticText (L"Strength", core::rect< s32 >(10,60, 80, 80), false, true, windowSettings);
     this->Gui->addEditBox (this->Player->getStat("strength").c_str(), core::rect<s32>(100,60,200,80), true, windowSettings);
 
+    this->Gui->addStaticText (L"Dexterity", core::rect< s32 >(10,90, 80, 110), false, true, windowSettings);
+    this->Gui->addEditBox (this->Player->getStat("dexterity").c_str(), core::rect<s32>(100,90,200,110), true, windowSettings);
+
+    this->Gui->addStaticText (L"Intelligence", core::rect< s32 >(10,120, 80, 140), false, true, windowSettings);
+    this->Gui->addEditBox (this->Player->getStat("intelligence").c_str(), core::rect<s32>(100,120,200,140), true, windowSettings);
+
+    this->Gui->addStaticText (L"Speed", core::rect< s32 >(10,150, 80, 170), false, true, windowSettings);
+    this->Gui->addEditBox (this->Player->getStat("speed").c_str(), core::rect<s32>(100,150,200,170), true, windowSettings);
+
+    this->Gui->addStaticText (L"Resistance", core::rect< s32 >(10,180, 80, 200), false, true, windowSettings);
+    this->Gui->addEditBox (this->Player->getStat("resistance").c_str(), core::rect<s32>(100,180,200,200), true, windowSettings);
+
+    this->Gui->addStaticText (L"Luck", core::rect< s32 >(10,210, 80, 230), false, true, windowSettings);
+    this->Gui->addEditBox (this->Player->getStat("luck").c_str(), core::rect<s32>(100,210,200,230), true, windowSettings);
+
+    this->Gui->addStaticText (L"Spirit", core::rect< s32 >(10,240, 80, 260), false, true, windowSettings);
+    this->Gui->addEditBox (this->Player->getStat("spirit").c_str(), core::rect<s32>(100,240,200,260), true, windowSettings);
+
+    this->Gui->addStaticText (L"Agility", core::rect< s32 >(10,270, 80, 290), false, true, windowSettings);
+    this->Gui->addEditBox (this->Player->getStat("agility").c_str(), core::rect<s32>(100,270,200,290), true, windowSettings);
+
     this->ButtonSave = this->Gui->addButton(
-            core::rect<s32>(80,250,150,270), windowSettings, 2, L"Save");
+            core::rect<s32>(80,350,150,370), windowSettings, 2, L"Save");
 
     this->ButtonExit = this->Gui->addButton(
-            core::rect<s32>(160,250,240,270), windowSettings, 2, L"Cancel and exit");
+            core::rect<s32>(160,350,240,370), windowSettings, 2, L"Cancel and exit");
 }

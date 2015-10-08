56d34a6e2813cda9aaaccf2e16e2a2699a29ecae
Bastien Leduc <b.leduc@student.isartdigital.com>
2015-10-08 15:50:04 +0200

    Sstruct created, contain Character stats

---
+++ include/Sstats.h
@@ -0,0 +1,17 @@
+#ifndef __SSTATS_H__
+#define __SSTATS_H__
+
+struct Sstats
+{
+    int strength,
+        dexterity,
+        intelligence,
+        speed,
+        resistance,
+        luck,
+        spirit,
+        agility;
+};
+
+
+#endif

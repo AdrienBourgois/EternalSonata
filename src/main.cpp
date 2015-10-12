4a5b23cecdf8f5e330448b97d00619bf14a86c56
Adrien Bourgois <a.bourgois@student.isartdigital.com>
2015-10-12 15:29:55 +0200

    Make a simple black window

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,30 @@
 using namespace irr;
 using namespace std;
 
-int main()
+int main(int, char*[])
 {
+    IrrlichtDevice* device = createDevice(video::EDT_OPENGL, {640,480}, 16, false, false, false, 0);
+
+    if (!device)
+        return EXIT_FAILURE;
+
+    device->setWindowCaption(L"Eternal Sonata");
+
+    video::IVideoDriver* driver = device->getVideoDriver();
+    scene::ISceneManager* smgr = device->getSceneManager();
+    gui::IGUIEnvironment* guienv = device->getGUIEnvironment();
+
+    while(device->run())
+    {
+        driver->beginScene(true, true);
+
+        smgr->drawAll();
+        guienv->drawAll();
+
+        driver->endScene();
+    }
+
+    device->drop();
 
     return EXIT_SUCCESS;
 }

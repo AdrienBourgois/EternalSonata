eb665d23f2bcdf163c0a811d652278de111c9488
Adrien Bourgois <a.bourgois@student.isartdigital.com>
2015-10-13 10:06:26 +0200

    Change map path and add detailed texture

--- src/main.cpp
+++ src/main.cpp
@@ -17,15 +17,14 @@ int main(int, char*[])
     scene::ISceneManager* smgr = device->getSceneManager();
     gui::IGUIEnvironment* guienv = device->getGUIEnvironment();
 
-    scene::ITerrainSceneNode* terrain = smgr->addTerrainSceneNode("media/terrain-heightmap.bmp", 0, -1, {0.f,0.f,0.f}, {0.f,0.f,0.f}, {40.f,1.f,40.f}, {255,255,255,255}, 1);
+    scene::ITerrainSceneNode* terrain = smgr->addTerrainSceneNode("assets/terrain-heightmap.bmp", 0, -1, {0.f,0.f,0.f}, {0.f,0.f,0.f}, {40.f,1.f,40.f}, {255,255,255,255}, 1);
     terrain->setMaterialFlag(video::EMF_LIGHTING, false);
+    terrain->setMaterialTexture(0, driver->getTexture("assets/terrain-texture.jpg"));
+    terrain->setMaterialTexture(1, driver->getTexture("assets/detailmap3.jpg"));
+    terrain->setMaterialType(video::EMT_DETAIL_MAP);
+    terrain->scaleTexture(1.0f, 20.0f);
 
-    terrain->setMaterialTexture(0,
-                driver->getTexture("media/terrain-texture.jpg"));
-
-    scene::ICameraSceneNode* camera =
-                smgr->addCameraSceneNodeFPS(0,100.0f,3.f);
-
+    scene::ICameraSceneNode* camera = smgr->addCameraSceneNodeFPS(0,100.0f,3.f);
     camera->setPosition(core::vector3df(2700*2,255*2,2600*2));
     camera->setTarget(core::vector3df(2397*2,343*2,2700*2));
     camera->setFarValue(42000.0f);

7d13921e868ef037b74cf2edcf6f6e1e61dc05d2
Bastien Leduc <b.leduc@student.isartdigital.com>
2015-10-14 10:27:52 +0200

    Changed members to full object. Delete Character assert.

--- include/Armor.h
+++ include/Armor.h
@@ -6,24 +6,24 @@
 class Armor
 {
     private:
-        Armor_Piece* head;
-        Armor_Piece* torso;
-        Armor_Piece* pants;
-        Armor_Piece* boots;
-        Armor_Piece* gloves;
+        Armor_Piece  head;
+        Armor_Piece  torso;
+        Armor_Piece  pants;
+        Armor_Piece  boots;
+        Armor_Piece  gloves;
 
     public:
-        Armor();
+        Armor() = default;
         Armor(Armor const&) = default;
         ~Armor() = default;
 
         Armor& operator =(Armor const&) = default;
 
-        void setHead(Armor_Piece* h)    {head = h;};
-        void setTorso(Armor_Piece* t)   {torso = t;};
-        void setPants(Armor_Piece* p)   {pants = p;};
-        void setBoots(Armor_Piece* b)   {boots = b;};
-        void setGloves(Armor_Piece* g)  {gloves = g;};
+        void setHead(Armor_Piece  h)    {head = h;};
+        void setTorso(Armor_Piece  t)   {torso = t;};
+        void setPants(Armor_Piece  p)   {pants = p;};
+        void setBoots(Armor_Piece  b)   {boots = b;};
+        void setGloves(Armor_Piece  g)  {gloves = g;};
 
         auto& getHead() const    {return head;};
         auto& getTorso() const   {return torso;};
--- src/Character.cpp
+++ src/Character.cpp
@@ -5,29 +5,21 @@
 Character::Character()
 {
     name = "Inideva";
-    stats = new Stats;
-    weapon = new Weapon;
-    armor = new Armor;
 
     level = 1;
-    stats->strength = 10;
-    stats->dexterity = 10;
-    stats->intelligence = 10;
-    stats->speed = 10;
-    stats->resistance = 10;
-    stats->luck = 10;
-    stats->spirit = 10;
-    stats->agility = 10;
-
-
+    stats.strength = 10;
+    stats.dexterity = 10;
+    stats.intelligence = 10;
+    stats.speed = 10;
+    stats.resistance = 10;
+    stats.luck = 10;
+    stats.spirit = 10;
+    stats.agility = 10;
 }
 
 std::ostream& operator <<(std::ostream& os, Character const& c)
 {
     using namespace std;
-
-    c.assertCharacter(c);
-
     os      << "[Character Debug]" << endl
             << "-Name: " << c.getName() << endl
             << "-Level: " << c.getLevel() << endl
@@ -39,31 +31,19 @@ std::ostream& operator <<(std::ostream& os, Character const& c)
             << "-Luck: " << c.getLuck() << endl
             << "-Spirit: " << c.getSpirit() << endl
             << "-Agility: " << c.getAgility() << endl 
-            << "-Weapon: " << c.getWeapon()->getName() << " (" << c.getWeapon()->getDamage() << ")" << endl
+            << "-Weapon: " << c.getWeapon().getName() << " (" << c.getWeapon().getDamage() << ")" << endl
             << "-Armor: " << endl
-            << "    -Head:      " << c.getArmor()->getHead()->getName() << " (RPhy: " << c.getArmor()->getHead()->getPhyDef() << ") (RMag: " << c.getArmor()->getHead()->getMagDef() << ")" << endl
-            << "    -Torso:     " << c.getArmor()->getTorso()->getName() << " (RPhy: " << c.getArmor()->getTorso()->getPhyDef() << ") (RMag: " << c.getArmor()->getTorso()->getMagDef() << ")" << endl
+            << "    -Head:      " << c.getArmor().getHead().getName() << " (RPhy: " << c.getArmor().getHead().getPhyDef() << ") (RMag: " << c.getArmor().getHead().getMagDef() << ")" << endl
+            << "    -Torso:     " << c.getArmor().getTorso().getName() << " (RPhy: " << c.getArmor().getTorso().getPhyDef() << ") (RMag: " << c.getArmor().getTorso().getMagDef() << ")" << endl
 
-            << "    -Gloves:    " << c.getArmor()->getGloves()->getName() << " (RPhy: " << c.getArmor()->getGloves()->getPhyDef() << ") (RMag: " << c.getArmor()->getGloves()->getMagDef() << ")" <<endl
+            << "    -Gloves:    " << c.getArmor().getGloves().getName() << " (RPhy: " << c.getArmor().getGloves().getPhyDef() << ") (RMag: " << c.getArmor().getGloves().getMagDef() << ")" <<endl
 
-            << "    -Pants:     " << c.getArmor()->getPants()->getName() << " (RPhy: " << c.getArmor()->getPants()->getPhyDef() << ") (RMag: " << c.getArmor()->getPants()->getMagDef() << ")" << endl
+            << "    -Pants:     " << c.getArmor().getPants().getName() << " (RPhy: " << c.getArmor().getPants().getPhyDef() << ") (RMag: " << c.getArmor().getPants().getMagDef() << ")" << endl
 
-            << "    -Boots:     " << c.getArmor()->getBoots()->getName() << " (RPhy: " << c.getArmor()->getBoots()->getPhyDef() << ") (RMag: " << c.getArmor()->getBoots()->getMagDef() << ")" << endl << endl;
+            << "    -Boots:     " << c.getArmor().getBoots().getName() << " (RPhy: " << c.getArmor().getBoots().getPhyDef() << ") (RMag: " << c.getArmor().getBoots().getMagDef() << ")" << endl << endl;
 
 
     return os;
-}
-
-void Character::assertCharacter(Character const& c) const
-{
-    assert( c.weapon && "WEAPON POINTER IS NULL !" );
-    assert( c.armor && "ARMOR POINTER IS NULL !" );
-    assert( c.armor->getHead() && "HEAD ARMOR POINTER IS NULL !");
-    assert( c.armor->getTorso() && "TORSO ARMOR POINTER IS NULL !");
-    assert( c.armor->getGloves() && "GLOVES ARMOR POINTER IS NULL !");
-    assert( c.armor->getPants() && "PANTS ARMOR POINTER IS NULL !");
-    assert( c.armor->getBoots() && "BOOTS ARMOR POINTER IS NULL !");
-
 }
 
 

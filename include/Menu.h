c264d32d6d02c2b70ebd783b1fcb28d81c4652bb
Adrien Bourgois <a.bourgois@student.isartdigital.com>
2015-10-22 11:59:40 +0200

    Add stats/player menu

--- include/Menu.h
+++ include/Menu.h
@@ -1,7 +1,10 @@
 #ifndef __MENU_INCLUDE__
 #define __MENU_INCLUDE__
 
+#include <string>
 #include "myIrrlicht.h"
+#include "EventReceiver.h"
+#include "Hero.h"
 
 using namespace irr;
 
@@ -15,7 +18,8 @@ public:
     void showPauseMenu();
     void closeMenu() {menuToBeDisplay = 0;
                       env->clear();}
-    //void showPlayerManu();
+    void showPlayerMenu(Hero);
+    void showStat(const wchar_t *, int, int, int);
     //void quitMenu();
 
     void setWindowSize(core::dimension2d<u32> size) { if (this->windowSize != size) 
@@ -24,8 +28,8 @@ public:
 
     void setResponsive() { this->window_width = this->windowSize.Width;
                            this->window_height = this->windowSize.Height;
-                           this->window_width_100 = this->window_width / 100;
-                           this->window_height_100 = this->window_height / 100; }
+                           this->w_w_100 = this->window_width / 100;
+                           this->w_h_100 = this->window_height / 100; }
 
     void setDevice(IrrlichtDevice* device) {this->device = device;
                                             this->driver = device->getVideoDriver();
@@ -42,8 +46,8 @@ private:
     bool newSize;
     int window_height;
     int window_width;
-    int window_height_100;
-    int window_width_100;
+    int w_h_100;
+    int w_w_100;
     int menuDisplay = 0;
     int menuToBeDisplay = 0;
 };
--- src/Menu.cpp
+++ src/Menu.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
-#include "myIrrlicht.h"
 #include "Menu.h"
-#include "EventReceiver.h"
 
 using namespace irr;
 
@@ -13,9 +11,9 @@ void Menu::showMainMenu()
     {
         env->clear();
         this->setResponsive();
-        env->addButton({40 * window_width_100, 50 * window_height_100, 60 * window_width_100, 60 * window_height_100},
+        env->addButton({40 * w_w_100, 50 * w_h_100, 60 * w_w_100, 60 * w_h_100},
                 0, GUI_ID_PLAY_BUTTON, L"Play");
-        env->addButton({40 * window_width_100, 70 * window_height_100, 60 * window_width_100, 80 * window_height_100},
+        env->addButton({40 * w_w_100, 70 * w_h_100, 60 * w_w_100, 80 * w_h_100},
                 0, GUI_ID_QUIT_BUTTON, L"Quit");
 
         this->menuDisplay = 1;
@@ -31,9 +29,9 @@ void Menu::showPauseMenu()
     {
         env->clear();
         this->setResponsive();
-        env->addButton({40 * window_width_100, 50 * window_height_100, 60 * window_width_100, 60 * window_height_100},
+        env->addButton({40 * w_w_100, 50 * w_h_100, 60 * w_w_100, 60 * w_h_100},
                 0, GUI_ID_PLAY_BUTTON, L"Resume");
-        env->addButton({40 * window_width_100, 70 * window_height_100, 60 * window_width_100, 80 * window_height_100},
+        env->addButton({40 * w_w_100, 70 * w_h_100, 60 * w_w_100, 80 * w_h_100},
                 0, GUI_ID_QUIT_BUTTON, L"Quit");
 
         this->menuDisplay = 1;
@@ -41,7 +39,7 @@ void Menu::showPauseMenu()
     }
 }
 
-/*void Menu::showPlayerMenu()
+void Menu::showPlayerMenu(Hero player)
 {
     setWindowSize(driver->getScreenSize());
     this->env = this->device->getGUIEnvironment();
@@ -49,13 +47,30 @@ void Menu::showPauseMenu()
     {
         env->clear();
         this->setResponsive();
-        env->addStaticText("", {40 * window_width_100, 70 * window_height_100, 60 * window_width_100, 80 * window_height_100}, false, true, 0, -1, true);
+        env->addStaticText(L"", {10 * w_w_100, 10 * w_h_100, 30 * w_w_100, 50 * w_h_100}, false, false, 0, -1, true);
+
+        showStat(L"Life :", player.getLife(), 11, 11);
+        showStat(L"Strength :", player.getStrength(), 11, 15);
+        showStat(L"Dexterity :", player.getDexterity(), 11, 19);
+        showStat(L"Intelligence :", player.getIntelligence(), 11, 23);
+        showStat(L"Speed :", player.getSpeed(), 11, 27);
+        showStat(L"Resistance :", player.getResistance(), 11, 31);
+        showStat(L"Luck :", player.getLuck(), 11, 35);
+        showStat(L"Spirit :", player.getSpirit(), 11, 39);
+        showStat(L"Agility :", player.getAgility(), 11, 43);
+
         this->menuDisplay = 1;
         this->newSize = false;
     }
 }
 
-void Menu::quitMenu()
+void Menu::showStat(const wchar_t * text, int value, int w, int h)
+{
+    env->addStaticText(text, {w * w_w_100, h * w_h_100, (w+10) * w_w_100, (h+2) * w_h_100});
+    env->addStaticText((core::stringw(value).c_str()), {(w+10) * w_w_100, h * w_h_100, (w+20) * w_w_100, (h+2) * w_h_100});
+}
+
+/*void Menu::quitMenu()
 {
     menuToBeDisplay = 0;
     env->clear();
